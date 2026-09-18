eb81c4e89398683a42fa56311340aa055a1ee72a
Peter Shoukry <peter.shoukry@udin.io>
2026-09-18 19:54:43 +0300

    chore: release 0.2.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@
 defmodule AshKotlinMultiplatform.MixProject do
   use Mix.Project
 
-  @version "0.1.3"
+  @version "0.2.0"
 
   @description """
   Generate type-safe Kotlin Multiplatform clients directly from your Ash resources and actions,
